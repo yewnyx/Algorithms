efdf8a6ba08248f968f013f94c0928fc535feb5c
yewnyx <yewnyx@yewnyx.xyz>
2024-07-14 16:01:23 -0700

    - Make a builder for the graph - Create a skip list on graph creation - Optimize graph iteration with skip list - Rename/change accessibility of private methods - Make edges an explicit (but readonly) struct type

--- Algorithms/Graph.cs
+++ Algorithms/Graph.cs
@@ -1,57 +1,106 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Yewnyx.Algorithms;
 
 public class Graph
 {
-    public Vertex[] Vertices;
-    public (int from, int to)[] Edges;
+    public sealed class Builder {
+        private SortedSet<Edge> _edges = new();
+        private int _maxIndex = -1;
 
-    public void Tarjan()
-    {
+        public Builder AddEdge(int from, int to) {
+            _edges.Add((from, to));
+            _maxIndex = Math.Max(_maxIndex, Math.Max(from, to));
+            return this;
+        }
+        
+        public Graph Build() {
+            var vLength = _maxIndex + 1;
+
+            var vertices = new Vertex[vLength];
+            var stack = new int[vLength];
+            var edges = new Edge[_edges.Count];
+            var nextEdge = new int[_edges.Count];
+            
+            Array.Fill(vertices, Vertex.New());
+            _edges.CopyTo(edges);
+
+            var previousFrom = -1;
+            var currentNextEdge = -1;
+            for (var i = _edges.Count - 1; i >= 0; i--) {
+                var from = edges[i].From;
+                if (from != previousFrom) {
+                    currentNextEdge = i;
+                    previousFrom = from;
+                }
+                nextEdge[i] = currentNextEdge + 1;
+                currentNextEdge = i;
+            }
+
+            return new Graph { Vertices = vertices, Edges = edges, _stack = stack, _stackIndex = 0, _edgeSkipList = nextEdge};
+        }
+    }
+
+    public Vertex[] Vertices;
+    public Edge[] Edges;
+
+    private int[] _stack;
+    private int _stackIndex;
+    private int[] _edgeSkipList;
+
+    public void Tarjan() {
         var vLength = Vertices.Length;
+
+        if (_stack == null || _stack.Length != vLength) { _stack = new int[vLength]; }
+        Array.Fill(_stack, -1);
+        _stackIndex = 0;
+
         var index = 0;
-        var stack = new ArrayStack(vLength);
-        for (var i = 0; i < vLength; i++)
-        {
-            if (Vertices[i].index == -1)
-            {
-                StrongConnect(i, ref index, stack);
+        for (var i = 0; i < vLength; i++) {
+            if (Vertices[i].index == -1) {
+                _strongConnect(i, ref index);
             }
         }
     }
 
-    private void StrongConnect(int vIndex, ref int index, ArrayStack stack) {
+    private void _push(int value) => _stack[_stackIndex++] = value;
+
+    private int _pop() => _stack[--_stackIndex];
+    
+    private void _strongConnect(int vIndex, ref int index) {
         ref var v = ref Vertices[vIndex];
         v.index = index;
         v.lowlink = index;
         index++;
-        stack.Push(vIndex);
+        _push(vIndex);
         v.onStack = true;
 
-        foreach (var (uIndex, wIndex) in Edges) {
-            if (uIndex == vIndex) { // Check if the edge starts from the current vertex
-                
+        // Loops over all edges from v; use skip list for better performance
+        var edgeIndex = 0;
+        while(edgeIndex < Edges.Length) {
+            var (uIndex, wIndex) = Edges[edgeIndex];
+            if (uIndex == vIndex) {
                 ref var w = ref Vertices[wIndex];
-                if (w.index == -1)
-                {
-                    StrongConnect(wIndex, ref index, stack);
-                    v.lowlink = System.Math.Min(v.lowlink, w.lowlink);
+                if (w.index == -1) {
+                    _strongConnect(wIndex, ref index);
+                    v.lowlink = Math.Min(v.lowlink, w.lowlink);
+                } else if (w.onStack) {
+                    v.lowlink = Math.Min(v.lowlink, w.index);
                 }
-                else if (w.onStack)
-                {
-                    v.lowlink = System.Math.Min(v.lowlink, w.index);
-                }
+                edgeIndex++;
+            } else {
+                edgeIndex = _edgeSkipList[edgeIndex];
             }
         }
 
         if (v.lowlink == v.index) {
+            // TODO: Optimize this
             var component = new List<int>();
-
             int wIndex;
             do {
-                wIndex = stack.Pop();
+                wIndex = _pop();
                 ref var w = ref Vertices[wIndex];
                 w.onStack = false;
                 component.Add(wIndex);
@@ -63,25 +112,7 @@ public class Graph
     }
 }
 
-public class ArrayStack
-{
-    public int[] array;
-    
-    public int index;
-
-    public ArrayStack(int capacity) {
-        array = new int[capacity];
-        Array.Fill(array, -1);
-        index = 0;
-    }
-
-    public void Push(int value) => array[index++] = value;
-
-    public int Pop() => array[--index];
-}
-
-public struct Vertex
-{
+public struct Vertex {
     public int index;
     public int lowlink;
     public bool onStack;
--- AlgorithmsRun/Program.cs
+++ AlgorithmsRun/Program.cs
@@ -24,33 +24,31 @@ digraph G {
   9 -> 12
 }
 """);
-var graph1 = new Graph {
-    Vertices = new Vertex[15],
-    Edges = [
-        (0, 1),
-        (1, 2),
-        (2, 3),
-        (3, 4),
-        (4, 5),
-        (5, 6),
-        (6, 7),
-        (7, 8),
-        (8, 9),
-        (9, 10),
-        (10, 11),
-        (11, 12),
-        (12, 13),
-        (13, 14),
-        (14, 9),  // This edge creates a cycle
-        (1, 4),
-        (3, 6),
-        (5, 8),
-        (7, 10),
-        (9, 12),
-    ]
-};
-Array.Fill(graph1.Vertices, Vertex.New());
+
+var graph1 = new Graph.Builder()
+    .AddEdge(0, 1)
+    .AddEdge(1, 2)
+    .AddEdge(2, 3)
+    .AddEdge(3, 4)
+    .AddEdge(4, 5)
+    .AddEdge(5, 6)
+    .AddEdge(6, 7)
+    .AddEdge(7, 8)
+    .AddEdge(8, 9)
+    .AddEdge(9, 10)
+    .AddEdge(10, 11)
+    .AddEdge(11, 12)
+    .AddEdge(12, 13)
+    .AddEdge(13, 14)
+    .AddEdge(14, 9) // This edge creates a cycle
+    .AddEdge(1, 4)
+    .AddEdge(3, 6)
+    .AddEdge(5, 8)
+    .AddEdge(7, 10)
+    .AddEdge(9, 12)
+    .Build();
 graph1.Tarjan();
+
 
 Console.WriteLine("""
 digraph G {
@@ -70,26 +68,22 @@ digraph G {
   8 -> 6
 }
 """);
-var graph2 = new Graph {
-    Vertices = new Vertex[10],
-    Edges = [
-        (0, 1),
-        (1, 2),
-        (2, 3),
-        (3, 4),
-        (4, 5),
-        (5, 6),
-        (6, 7),
-        (7, 8),
-        (8, 9),
-        (0, 3),
-        (1, 4),
-        (5, 8),
-        (7, 9),
-        (8, 6),
-    ]
-};
-Array.Fill(graph2.Vertices, Vertex.New());
+var graph2 = new Graph.Builder()
+    .AddEdge(0, 1)
+    .AddEdge(1, 2)
+    .AddEdge(2, 3)
+    .AddEdge(3, 4)
+    .AddEdge(4, 5)
+    .AddEdge(5, 6)
+    .AddEdge(6, 7)
+    .AddEdge(7, 8)
+    .AddEdge(8, 9)
+    .AddEdge(0, 3)
+    .AddEdge(1, 4)
+    .AddEdge(5, 8)
+    .AddEdge(7, 9)
+    .AddEdge(8, 6)
+    .Build();
 graph2.Tarjan();
 
 Console.WriteLine("""
@@ -111,24 +105,20 @@ digraph G {
   8 -> 8
 }
 """);
-var graph3 = new Graph {
-    Vertices = new Vertex[9],
-    Edges = [
-        (1, 2),
-        (2, 3),
-        (3, 1),
-        (4, 2),
-        (4, 3),
-        (4, 5),
-        (5, 4),
-        (5, 6),
-        (6, 3),
-        (6, 7),
-        (7, 6),
-        (8, 5),
-        (8, 7),
-        (8, 8),
-    ]
-};
-Array.Fill(graph3.Vertices, Vertex.New());
+var graph3 = new Graph.Builder()
+    .AddEdge(1, 2)
+    .AddEdge(2, 3)
+    .AddEdge(3, 1)
+    .AddEdge(4, 2)
+    .AddEdge(4, 3)
+    .AddEdge(4, 5)
+    .AddEdge(5, 4)
+    .AddEdge(5, 6)
+    .AddEdge(6, 3)
+    .AddEdge(6, 7)
+    .AddEdge(7, 6)
+    .AddEdge(8, 5)
+    .AddEdge(8, 7)
+    .AddEdge(8, 8)
+    .Build();
 graph3.Tarjan();
